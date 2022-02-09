b6d3168ea7926ab031c5b559e46d93e8dd0cae6f
somak01 <dbksomak@gmail.com>
2022-02-09 21:59:10 +0100

    test

---
+++ vsjava/InfinityNNanTest.java
@@ -0,0 +1,17 @@
+public class InfinityNNanTest 
+{
+    public static void main(String[] args)
+    {
+        double x = Double.NaN;
+        if (Double.isNaN(x))
+        {
+            System.out.println("Yeet!");
+        }
+        else
+        {
+            System.out.println("The prophecy is true!");
+        }
+        System.out.println("\u2122 \n\u03C0");
+        
+    }
+}
